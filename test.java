00f1d7bd4a683f72ecd5c8eef6ff313ca4466149
sinwaves <sinwave191@gmail.com>
2023-09-25 14:42:47 +0900

    コードを修正しました9/25

---
+++ test.java
@@ -0,0 +1,17 @@
+public class test{
+	
+	public static void main(String[]args) 
+    {
+		System.out.println("やっはろーわーるど");
+        
+        String [] zonu = { 　　　 ／￣￣￣￣＼  , 　　／　　　　 ●　　●､ , 　　|Y　　Y　　　　　　　 ＼, 　　| |　　 | 　　　　　　 ▼ |, 　　| ＼／　　　　　　＿人.|  , 　　| ＼／　　　　　　＿人.| , 　　|　　　　　　 ＿＿＿ノ , 　　＼　　　 .／   , 　　　｜ ｜　|   , 　　　（_＿）＿）}
+
+         for(String i : zonu[]){
+            System.out.println(i);
+         }
+		
+		
+	}
+	
+	
+}
