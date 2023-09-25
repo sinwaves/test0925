733e6ba383a69be183d8ac598c5e00dfae8b4b28
sinwaves <145947751+sinwaves@users.noreply.github.com>
2023-09-25 14:55:36 +0900

    Update test.java

--- test.java
+++ test.java
@@ -3,11 +3,8 @@ public class test{
 	public static void main(String[]args) 
     {
 		System.out.println("やっはろーわーるど");
-        
-        String [] zonu = { 　　　 ／￣￣￣￣＼  , 　　／　　　　 ●　　●､ , 　　|Y　　Y　　　　　　　 ＼, 　　| |　　 | 　　　　　　 ▼ |, 　　| ＼／　　　　　　＿人.|  , 　　| ＼／　　　　　　＿人.| , 　　|　　　　　　 ＿＿＿ノ , 　　＼　　　 .／   , 　　　｜ ｜　|   , 　　　（_＿）＿）}
-
-         for(String i : zonu[]){
-            System.out.println(i);
+        	System.out.println("githubから更新");
+		
          }
 		
 		
